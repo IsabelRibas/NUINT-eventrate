2943b11c8c4a7ce3370f70f1e83b8091d6372619
IsabelRibas <145351220+IsabelRibas@users.noreply.github.com>
2024-04-02 11:25:31 -0300

    Update make_binnedgraph_mu.C

--- make_binnedgraph_mu.C
+++ make_binnedgraph_mu.C
@@ -167,7 +167,7 @@ void make_binnedgraph_mu()
     //Criando legenda
     TLegend *legend = new TLegend(0.7, 0.7, 0.9, 0.9);
     legend->AddEntry(hist, "Standard", "f");
-    legend->AddEntry(histBSM_2pi, "#phi_{#mu#tau} = 2#pi", "f");
+    legend->AddEntry(histBSM_2pi, "#phi_{#mu#tau} = 0", "f");
     legend->AddEntry(histBSM_3pi5, "#phi_{#mu#tau} = 3#pi/2", "f");
     legend->AddEntry(histBSM_pi, "#phi_{#mu#tau} = #pi", "f");
     legend->AddEntry(histBSM_pi5, "#phi_{#mu#tau} = #pi/2", "f");
